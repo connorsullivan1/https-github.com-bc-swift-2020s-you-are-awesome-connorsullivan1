e0281756ad8dfcc462466964ed5f6d69b192840b
Connor Sullivan <sulliamr@bc.edu>
2020-01-18 16:47:28 -0500

    no changes

--- Two Button Challenge/ViewController.swift
+++ Two Button Challenge/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
